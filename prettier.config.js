604718c5fec90d55d4f7b285ca4e3e90bd407efc
Xiradorn <xiradorn@gmail.com>
2024-10-21 23:24:50 +0200

    feat: :circus_tent: First commit

---
+++ prettier.config.js
@@ -0,0 +1,12 @@
+/**
+ * @type {import("prettier").Config}
+ */
+const config = {
+	semi: false,
+	trailingComma: 'all',
+	useTabs: true,
+	tabWidth: 4,
+	singleQuote: true,
+}
+
+export default config
